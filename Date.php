8f42e7a46f6f4201767ca3a745ea2c36943e6c87
Eduardo Turiño <eturino@gmail.com>
2012-06-24 18:20:37 +0200

    date apps

--- Date.php
+++ Date.php
@@ -6,6 +6,117 @@
  */
 class EtuDev_Util_Date {
 
+
+	/**
+	 * set DateTime to TODAY, but keeping the time data
+	 *
+	 * @param DateTime $dt
+	 * @param bool     $mutable if false, create a new DateTime using clone($dt); if else then use same object
+	 *
+	 * @return DateTime
+	 */
+	static public function dateTimeSetToday(DateTime $dt, $mutable = true) {
+		$a = getdate();
+		if ($mutable) {
+			$x = $dt;
+		} else {
+			$x = clone($dt);
+		}
+		$x->setDate($a['year'], $a['mon'], $a['mday']);
+		return $x;
+	}
+
+
+	/**
+	 * @static
+	 * @throws Exception
+	 *
+	 * @param int|DateTime             $minHour
+	 * @param DateTimeZone|string|null $timezone
+	 *
+	 * @return DateTime|null
+	 */
+	static public function getDateTimeNowPlusHours($minHour, $timezone = null) {
+		$tz = self::getDateTimeZone($timezone);
+		/** @var $mh DateTime */
+		if ($minHour instanceof DateTime) {
+			$mh = $minHour;
+			static::dateTimeSetToday($mh);
+			return $mh;
+		}
+
+		if (is_numeric($minHour) && $minHour > 0) {
+			$mh_entero  = floor($minHour);
+			$mh_decimal = $minHour - $mh_entero;
+
+			if ($mh_decimal) {
+				$mh_minutos = floor($mh_decimal * 60);
+			} else {
+				$mh_minutos = 0;
+			}
+
+			$mh = static::getDateTimeToday(true, $tz);
+			$mh->add(new DateInterval('PT' . $mh_entero . 'H' . ($mh_minutos ? $mh_minutos . 'M' : '')));
+
+			unset($mh_entero);
+			unset($mh_decimal);
+			unset($mh_minutos);
+
+			return $mh;
+		}
+
+		if ($minHour) {
+			$mh = DateTime::createFromFormat('H:i', $minHour, $tz);
+			static::dateTimeSetToday($mh);
+			if (!$mh) {
+				throw new Exception('invalid minhour, needs to be in H:i format');
+			}
+
+			return $mh;
+		}
+
+		return null;
+	}
+
+
+	/**
+	 * @static
+	 *
+	 * @param bool         $with_time
+	 * @param DateTimeZone $timezone
+	 *
+	 * @return DateTime
+	 */
+	static public function getDateTimeToday($with_time = false, DateTimeZone $timezone = null) {
+		$d = new DateTime('now', $timezone ?: static::getDateTimeZone());
+		if (!$with_time) {
+			$d->setTime(0, 0, 0);
+		}
+		return $d;
+	}
+
+
+	/**
+	 * @static
+	 * @param string|DateTimeZone $tz the actual TimeZone, or a TimeZone description to use in the DateTimeZone constructor, if there is none, use UTC
+	 * @return DateTimeZone
+	 */
+	static public function getDateTimeZone($tz = null){
+		if($tz instanceof DateTimeZone){
+			return $tz;
+		}
+
+		if(!is_string($tz) && $tz['timezone']){
+			$tz = $tz['timezone'];
+		}
+
+		if(!$tz || !is_string($tz)){
+			$tz = EtuDev_Zend_Util_App::getZFConfig('timezone') ?: 'UTC';
+		}
+
+		return new DateTimeZone($tz);
+	}
+
 	/**
 	 * Get the current datetime string
 	 *
@@ -53,23 +164,46 @@ class EtuDev_Util_Date {
 
 
 	/**
-	 * @static
-	 * @param string|DateTimeZone $tz the actual TimeZone, or a TimeZone description to use in the DateTimeZone constructor, if there is none, use UTC
-	 * @return DateTimeZone
+	 * checks if the given string represents a date in the format "YYYY-MM-DD" or "YYYY-MM-DD HH:MM:SS"
+	 *
+	 * @param string $string
+	 *
+	 * @return bool
 	 */
-	static public function getDateTimeZone($tz = null){
-		if($tz instanceof DateTimeZone){
-			return $tz;
+	static public function isDate($string) {
+		if (!$string) {
+			return false;
 		}
 
-		if(!is_string($tz) && $tz['timezone']){
-			$tz = $tz['timezone'];
+		if (preg_match('/^[\d]{4}-[\d]{2}-[\d]{2}$/', $string)) {
+			return true;
 		}
 
-		if(!$tz || !is_string($tz)){
-			$tz = defined('BR_DEFAULT_TIMEZONE') ? BR_DEFAULT_TIMEZONE : 'UTC';
+		if (preg_match('/^[\d]{4}-[\d]{2}-[\d]{2} [\d]{2}:[\d]{2}$/', $string)) {
+			return true;
 		}
 
-		return new DateTimeZone($tz);
+		if (preg_match('/^[\d]{4}-[\d]{2}-[\d]{2} [\d]{2}:[\d]{2}:[\d]{2}$/', $string)) {
+			return true;
+		}
+
+		return false;
 	}
+
+
+	static public function getDateSection($string) {
+		$matches = array();
+		if (preg_match('/^[\d]{4}-[\d]{2}-[\d]{2}$/', $string, $matches)) {
+			return $string;
+		}
+
+		if (preg_match('/^[\d]{4}-[\d]{2}-[\d]{2} /', $string, $matches)) {
+			if ($matches) {
+				return trim($matches[0]);
+			}
+		}
+
+		return null;
+	}
+
 }
