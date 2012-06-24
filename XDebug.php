9fdfe1bce62f9f55ca44d6af44d181e0dd91f17e
Eduardo Turiño <eturino@gmail.com>
2012-06-24 02:18:43 +0200

    static xdebug

--- XDebug.php
+++ XDebug.php
@@ -2,29 +2,29 @@
 
 class EtuDev_Util_XDebug {
 
-	function setVarDumpNormalDepth() {
+	static public function setVarDumpNormalDepth() {
 		ini_set('xdebug.var_display_max_depth', 3);
 	}
 
-	function setVarDumpHighDepth() {
+	static public function setVarDumpHighDepth() {
 		ini_set('xdebug.var_display_max_depth', 5);
 		ini_set('xdebug.var_display_max_data', 10240);
 		ini_set('xdebug.var_display_max_children', 10240);
 	}
 
-	function setVarDumpHigherDepth() {
+	static public function setVarDumpHigherDepth() {
 		ini_set('xdebug.var_display_max_depth', 9);
 		ini_set('xdebug.var_display_max_data', 10240);
 		ini_set('xdebug.var_display_max_children', 10240);
 	}
 
-	function setVarDumpHighestDepth() {
+	static public function setVarDumpHighestDepth() {
 		ini_set('xdebug.var_display_max_depth', 15);
 		ini_set('xdebug.var_display_max_data', 10240);
 		ini_set('xdebug.var_display_max_children', 10240);
 	}
 
-	function setVarDumpDepth($depth = 3) {
+	static public function setVarDumpDepth($depth = 3) {
 		ini_set('xdebug.var_display_max_depth', $depth);
 	}
 
