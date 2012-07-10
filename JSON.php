b9697ef4ebb10e335197ed4ee4fec4cbf3b2c7af
Eduardo Turiño <eturino@gmail.com>
2012-07-10 23:39:59 +0200

    json

--- JSON.php
+++ JSON.php
@@ -3,6 +3,9 @@
 class EtuDev_Util_JSON {
 
 	/**
+	 * use single quote (') in the attribute
+	 * ie: <input data-myjson='$ENCODED' />
+	 *
 	 * @static
 	 *
 	 * @param mixed $text
@@ -10,14 +13,17 @@ class EtuDev_Util_JSON {
 	 * @return null|string
 	 */
 	static public function encodetoHTMLAttribute($text) {
-		try{
-			return addcslashes(json_encode($text, JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP | JSON_UNESCAPED_UNICODE), '"');
-		}catch (Exception $ex){
+		try {
+			return json_encode($text, JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP | JSON_UNESCAPED_UNICODE);
+		} catch (Exception $ex) {
 			return null;
 		}
 	}
 
 	/**
+	 * use single quote (') in the attribute
+	 * ie: <input data-myjson='$ENCODED' />
+	 *
 	 * @static
 	 *
 	 * @param $text
@@ -25,9 +31,9 @@ class EtuDev_Util_JSON {
 	 * @return null|string
 	 */
 	static public function encodetoHTMLAttributeString($text) {
-		try{
-			return addcslashes(json_encode((string) $text, JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP | JSON_UNESCAPED_UNICODE), '"');
-		}catch (Exception $ex){
+		try {
+			return json_encode((string) $text, JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP | JSON_UNESCAPED_UNICODE);
+		} catch (Exception $ex) {
 			return null;
 		}
 	}
@@ -40,9 +46,9 @@ class EtuDev_Util_JSON {
 	 * @return null|string
 	 */
 	static public function encode($text) {
-		try{
+		try {
 			return json_encode($text);
-		}catch (Exception $ex){
+		} catch (Exception $ex) {
 			return null;
 		}
 	}
