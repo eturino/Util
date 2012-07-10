9400a12355de383cc849ddd2f1f1c863e31173dc
Eduardo Turiño <eturino@gmail.com>
2012-07-10 23:27:04 +0200

    json

--- JSON.php
+++ JSON.php
@@ -5,13 +5,28 @@ class EtuDev_Util_JSON {
 	/**
 	 * @static
 	 *
+	 * @param mixed $text
+	 *
+	 * @return null|string
+	 */
+	static public function encodetoHTMLAttribute($text) {
+		try{
+			return addcslashes(json_encode($text, JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP | JSON_UNESCAPED_UNICODE), '"');
+		}catch (Exception $ex){
+			return null;
+		}
+	}
+
+	/**
+	 * @static
+	 *
 	 * @param $text
 	 *
 	 * @return null|string
 	 */
-	static public function encode_to_attribute($text) {
+	static public function encodetoHTMLAttributeString($text) {
 		try{
-			return json_encode((string) $text, JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP | JSON_UNESCAPED_UNICODE);
+			return addcslashes(json_encode((string) $text, JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP | JSON_UNESCAPED_UNICODE), '"');
 		}catch (Exception $ex){
 			return null;
 		}
@@ -26,7 +41,7 @@ class EtuDev_Util_JSON {
 	 */
 	static public function encode($text) {
 		try{
-			return json_encode((string) $text);
+			return json_encode($text);
 		}catch (Exception $ex){
 			return null;
 		}
