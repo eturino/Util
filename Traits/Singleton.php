08cb266fb2795746a062618e466352362e03139f
Eduardo Turiño <eturino@gmail.com>
2013-02-20 10:37:45 +0100

    docblock

--- Traits/Singleton.php
+++ Traits/Singleton.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * makes de class a singleton like class (getInstance Method)
+ *
+ * needs to put in the docblock the @method line: ˝@method static ActualClassName getInstance()˝ so that IDEs know the actual class
+ *
+ */
 trait EtuDev_Util_Traits_Singleton {
 	static private $_singleton_instance;
 
